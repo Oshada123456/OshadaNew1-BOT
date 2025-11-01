86cef79ca14a850680c92a38b5213c3af60c6e3d
Oshada123456 <Oaluthgedara@gmail.com>
2025-11-01 22:53:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "MdFhFB5J#hk69apv0u1YM58X_FFlUb_NFmfWb7pqL6p1N0XJUDzA",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Oshada123456/OshadaNew1-BOT/blob/main/images/Picsart_25-04-07_21-41-15-367.png?raw=true",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Oshada123456/OshadaNew1-BOT/blob/main/images/Picsart_25-04-03_21-30-50-609.jpg?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 Oshi-BOT Is Alive Now😍*",
 BOT_OWNER: '94788345811',  // Replace with the owner's phone number
 
